fc02de4b3bc42789bc4e7438d777e5dc99b0c6b6
AlleyChang <yellaatthebeach@gmail.com>
2025-03-03 06:06:40 +0800

    [feat] add form submit

--- src/main.js
+++ src/main.js
@@ -4,6 +4,12 @@ import VueAxios from 'vue-axios';
 import Loading from 'vue3-loading-overlay';
 import 'vue3-loading-overlay/dist/vue3-loading-overlay.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import {
+  Form, Field, ErrorMessage, defineRule, configure,
+} from 'vee-validate';
+import * as AllRules from '@vee-validate/rules';
+import { localize, setLocale } from '@vee-validate/i18n';
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json';
 import App from './App.vue';
 import router from './router';
 import { currency, date } from './methods/filters';
@@ -13,10 +19,27 @@ const app = createApp(App);
 app.config.globalProperties.$filters = {
   currency, date,
 };
+Object.keys(AllRules).forEach((rule) => {
+  console.log(typeof rule);
+  console.log(rule);
+  if (typeof AllRules[rule] === 'function') {
+    defineRule(rule, AllRules[rule]);
+  }
+});
+
+configure({
+  validateOnInput: true, // 當輸入任何內容直接進行驗證
+  generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
+});
+// 設定預設語系
+setLocale('zh_TW');
 
 // app.config.globalProperties.$httpMessageState = $httpMessageState;
 app.provide('$httpMessageState', $httpMessageState);
 app.use(VueAxios, axios);
 app.use(router);
 app.component('Loading', Loading);
+app.component('Form', Form);
+app.component('Field', Field);
+app.component('ErrorMessage', ErrorMessage);
 app.mount('#app');
--- src/views/UserCart.vue
+++ src/views/UserCart.vue
@@ -114,6 +114,61 @@
         </div>
       </div>
     </div>
+    <!-- form -->
+    <div class="my-5 row justify-content-center">
+      <Form class="col-md-6" v-slot="{ errors }"  @submit="createOrder">
+        <div class="mb-3">
+          <span id="email-label">Email</span>
+          <Field id="email" name="email" type="email" class="form-control"
+          aria-labelledby="email-label"
+          v-model="form.user.email"
+          :class="{ 'is-invalid': errors['email'] }"
+                   placeholder="請輸入 Email" rules="email|required"
+                   ></Field>
+          <ErrorMessage name="email-label" class="invalid-feedback"></ErrorMessage>
+        </div>
+
+        <div class="mb-3">
+          <span id="name-label">收件人姓名</span>
+          <Field id="name" name="姓名" type="text" class="form-control" aria-labelledby="name-label"
+          v-model="form.user.name"
+          :class="{ 'is-invalid': errors['姓名'] }"
+                   placeholder="請輸入姓名" rules="required"
+                  ></Field>
+          <ErrorMessage name="姓名" class="invalid-feedback"></ErrorMessage>
+        </div>
+
+        <div class="mb-3">
+           <span id="tel-label">收件人電話</span>
+          <Field id="tel" name="電話" type="tel" class="form-control" aria-labelledby="tel-label"
+                   :class="{ 'is-invalid': errors['電話'] }"
+                   placeholder="請輸入電話" rules="required"
+                   v-model="form.user.tel" ></Field>
+          <ErrorMessage name="電話" class="invalid-feedback"></ErrorMessage>
+        </div>
+
+        <div class="mb-3">
+          <span id="address-label">收件人地址</span>
+          <Field id="address" name="地址" type="text" class="form-control"
+          aria-labelledby="address-label"
+                   :class="{ 'is-invalid': errors['地址'] }"
+                   placeholder="請輸入地址" rules="required"
+                   v-model="form.user.address"></Field>
+          <ErrorMessage name="地址" class="invalid-feedback"></ErrorMessage>
+        </div>
+
+        <div class="mb-3">
+          <label for="message" class="form-label">留言
+          <textarea name="" id="message" class="form-control" cols="30" rows="10"
+                    v-model="form.message">
+          </textarea>
+          </label>
+        </div>
+        <div class="text-end">
+          <button class="btn btn-danger">送出訂單</button>
+        </div>
+      </Form>
+    </div>
   </div>
 </template>
 
@@ -129,6 +184,15 @@ export default {
       },
       cart: {},
       couponCode: '',
+      form: {
+        user: {
+          name: '',
+          email: '',
+          tel: '',
+          address: '',
+        },
+        message: '',
+      },
     };
   },
   inject: {
@@ -206,6 +270,14 @@ export default {
         this.getCart();
         this.isLoading = false;
       });
+    },
+    createOrder() {
+      const url = `${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/order`;
+      const order = this.form;
+      this.$http.post(url, { data: order })
+        .then((res) => {
+          console.log(res);
+        });
     },
   },
   created() {
